5b4d3af80e888f96022b0d4dd48f518fdec5b5f2
chellay27 <ajay.ch1@gmail.com>
2025-03-09 00:17:03 +0530

    JS Executor tool

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -19,6 +19,49 @@ const weatherTool = tool(
   }
 );
 
+async function evalAndCaptureOutput(code) {
+  const oldLog = console.log;
+  const oldError = console.error;
+
+  const output = [];
+  let errorOutput = [];
+
+  console.log = (...args) => output.push(args.join(" "));
+  console.error = (...args) => errorOutput.push(args.join(" "));
+
+  try {
+    await eval(code);
+  } catch (error) {
+    errorOutput.push(error.message);
+  }
+
+  console.log = oldLog;
+  console.error = oldError;
+
+  return { stdout: output.join("\n"), stderr: errorOutput.join("\n") };
+}
+
+const jsExecutor = tool(
+  async ({ code }) => {
+    const result = await evalAndCaptureOutput(code);
+
+    return result;
+  },
+  {
+    name: "run_javascript_code_tool",
+    description: `
+      Run general purpose javascript code. 
+      This can be used to access Internet or do any computation that you need. 
+      The output will be composed of the stdout and stderr. 
+      The code should be written in a way that it can be executed with javascript eval in node environment.
+      For weather fetch data from openweather api. api key is 270f17df34305bc532aa480a3cec14fe
+    `,
+    schema: z.object({
+      code: z.string().describe("code to be executed"),
+    }),
+  }
+);
+
 const model = new ChatAnthropic({
   model: "claude-3-5-sonnet-latest",
 });
@@ -27,6 +70,6 @@ const checkpointSaver = new MemorySaver();
 
 export const agent = createReactAgent({
   llm: model,
-  tools: [weatherTool],
+  tools: [jsExecutor],
   checkpointSaver,
 });
